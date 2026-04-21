9f3469f58ad0ab02b1c1f543a4d44d6b017b7cc0
linthit23 <linthitkhant7@gmail.com>
2026-04-21 16:14:38 +0700

    Initial Commit

---
+++ MoodTracker-iOSTests/MoodTracker_iOSTests.swift
@@ -0,0 +1,19 @@
+//
+//  MoodTracker_iOSTests.swift
+//  MoodTracker-iOSTests
+//
+//  Created by Lin Thit on 4/21/26.
+//
+
+import Testing
+@testable import MoodTracker_iOS
+
+struct MoodTracker_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
